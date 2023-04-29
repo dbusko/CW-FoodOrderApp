cdb5da928a7ed24ee809ef1d7805b45c795b0f20
dmytrobusko1106@gmail.com <dmytrobusko1106@gmail.com>
2023-04-29 12:19:12 +0300

    Upgrade: Dashboard v.3.3

--- client/src/components/DBItems.jsx
+++ client/src/components/DBItems.jsx
@@ -1,10 +1,63 @@
 import React from "react";
 import { DataTable } from "../components";
+import { HiCurrencyRupee } from "../assets/icons";
+import { useSelector } from "react-redux";
 
 const DBItems = () => {
+  const products = useSelector((state) => state.products);
   return (
     <div className="flex items-center justify-self-center gap-4 pt-6 w-full">
-      <DataTable />
+      <DataTable
+        columns={[
+          {
+            title: "Image",
+            field: "imageURL",
+            render: (rowData) => (
+              <img
+                src={rowData.imageURL}
+                className="w-32 h-16 object-contain rounded-md"
+              />
+            ),
+          },
+          {
+            title: "Name",
+            field: "product_name",
+          },
+          {
+            title: "Category",
+            field: "product_category",
+          },
+          {
+            title: "Price",
+            field: "product_price",
+            render: (rowData) => (
+              <p className="text-xl font-semibold text-textColor flex items-center justify-center">
+                <HiCurrencyRupee className="text-red-400" />{" "}
+                {/* <span className="text-red-400">$</span> */}
+                {parseFloat(rowData.product_price).toFixed(2)}
+              </p>
+            ),
+          },
+        ]}
+        data={products}
+        title="List of Products"
+        actions={[
+          {
+            icon: "edit",
+            tooltip: "Edit Data",
+            onClick: (event, rowData) => {
+              alert("You want to edit " + rowData.productId);
+            },
+          },
+          {
+            icon: "delete",
+            tooltip: "Delete Data",
+            onClick: (event, rowData) => {
+              alert("You want to delete " + rowData.productId);
+            },
+          },
+        ]}
+      />
     </div>
   );
 };
