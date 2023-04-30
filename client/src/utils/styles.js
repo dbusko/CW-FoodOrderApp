bf74220ea5b8a6a39abbbd3960ab4b9877c0a944
dmytrobusko1106@gmail.com <dmytrobusko1106@gmail.com>
2023-04-30 15:11:34 +0300

    Upgrade: Home

--- client/src/utils/styles.js
+++ client/src/utils/styles.js
@@ -13,3 +13,55 @@ export const statuses = [
   { id: 6, title: "Chinese", category: "chinese" },
   { id: 7, title: "Bread", category: "bread" },
 ];
+
+export const foodInfo = [
+  {
+    id: 1,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/cw-foodorderapp.appspot.com/o/Images%2F1682855063665_i1.png?alt=media&token=e9cb094d-4f5c-43cc-b346-93d3d6d8830e",
+    product_name: "Морозиво",
+    product_category: "Шоколад & Ваніль",
+    product_price: "70.00",
+  },
+  {
+    id: 2,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/cw-foodorderapp.appspot.com/o/Images%2F1682856446684_f1.png?alt=media&token=12043666-90a2-4d86-b4b7-dbce657ec7b2",
+    product_name: "Полуниця",
+    product_category: "Свіжа Полуниця",
+    product_price: "200.00",
+  },
+  {
+    id: 3,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/cw-foodorderapp.appspot.com/o/Images%2F1682856470880_f2.png?alt=media&token=1bf85b24-322a-4b43-af6e-210c692f97f1",
+    product_name: "Ананас",
+    product_category: "Соковитий Ананас",
+    product_price: "159.00",
+  },
+  {
+    id: 4,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/cw-foodorderapp.appspot.com/o/Images%2F1682856504193_c3.png?alt=media&token=7875080a-3ab1-48b2-8a08-35f667e94b37",
+    product_name: "Курятина",
+    product_category: "Куряча тарілка",
+    product_price: "150.00",
+  },
+  {
+    id: 5,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/cw-foodorderapp.appspot.com/o/Images%2F1682856532302_d4.png?alt=media&token=5e65966f-5631-4d38-a003-1710afc84f7b",
+    product_name: "Мохіто",
+    product_category: "Коктейль",
+    product_price: "88.00",
+  },
+
+  {
+    id: 6,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/cw-foodorderapp.appspot.com/o/Images%2F1682855080705_cu3.png?alt=media&token=5667caf4-93f8-47f5-9ca9-0a714eb89da6",
+    product_name: "Суп",
+    product_category: "Морепродукти",
+    product_price: "120.00",
+  },
+];
